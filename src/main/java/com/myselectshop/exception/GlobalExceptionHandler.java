ae0085ca2b48d571a01134ab9786d6d0bdb1bc1b
kimboseong <njunju77@nate.com>
2024-08-13 16:29:50 +0900

    exception handling

---
+++ src/main/java/com/myselectshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.myselectshop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException ex) {
+        ErrorResponse restApiException = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+}
